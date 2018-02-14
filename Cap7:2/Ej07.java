c7d31d7f78e2c4a9a4ed0e8b489d896fc4011537
Viktoriia Stasiuk <viktoriiaStasiuk@github.com>
2018-02-14 20:54:15 +0100

    ejercicios7

--- Cap7:2/Ej07.java
+++ Cap7:2/Ej07.java
@@ -2,7 +2,6 @@
  *
  * 7. Mejora el juego "Busca el tesoro" de tal forma que si hay una mina a una
  *    casilla de distancia, el programa avise diciendo ¡Cuidado! ¡Hay una mina cerca!
- * 
  */
 public class Ej07 {
 
